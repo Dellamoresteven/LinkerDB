5fe33bc4c53baf4950662626a536b7bca7197b5f
Steven Dellamore <dellamoresteven@gmail.com>
2019-05-09 17:21:25 -0500

    documentation

--- system/database.cc
+++ system/database.cc
@@ -65,7 +65,6 @@ int main(void){
 
     /* init new command to COMMAND_START */
     int stage = TABLE_LOOKUP;
-
     /* Runs through all the tokens */
     for(int i = 0; i < toks.size(); i++){
       /* Prints the DB's (For testing Only) */
@@ -93,9 +92,13 @@ int main(void){
          * This will make Rhythm->user->steven exist
          */
         case NEWLINK: {
+          /* Sets up new table */
           table_t * t = new table_t;
+          /* adds the table name */
           t->table_name = toks.at(i);
+          /* inserts it in the tableHead */
           headTable->linked_table_names.insert(std::make_pair(toks.at(i), t));
+          /* Done! */
           break;
         }
 
@@ -105,11 +108,17 @@ int main(void){
          * THIS FUNCTION IS NOT YET WORKING SORZ
          */
         case PRINTSTRUC: {
+          /* Gets the link you are trying to print the structure of */
           headTable = tableSearch(toks.at(i));
+          /* Sets the headpath to the new value */
           headTablePath = toks.at(i);
+          /* Checks to make sure it exists */
           if(headTable->table_name == "-1"){ toks.clear(); break; }
+          /* Sends in to the printStructure function in print.cc */
           printStructure(headTable,5);
+          /* Modifies the stage once print is done */
           stage = TABLE_LOOKUP;
+          /* Done! */
           break;
         }
 
@@ -123,11 +132,17 @@ int main(void){
          */
         case PRINT: {
           if(PRINT_DEBUG) printf("PRINT: %s\n", toks.at(i).c_str());
+          /* Gets the link to which you want to print */
           headTable = tableSearch(toks.at(i));
+          /* Sets the headpath to the new value */
           headTablePath = toks.at(i);
+          /* Checks to make sure it exists */
           if(headTable->table_name == "-1"){ toks.clear(); break; }
+          /* Sends in to the printTable function in print.cc */
           printTable(headTable);
+          /* Modifies the stage once print is done */
           stage = TABLE_LOOKUP;
+          /* Done! */
           break;
         }
 
@@ -140,10 +155,15 @@ int main(void){
          * prints all links inside of Rhythm, user link
          */
         case DESC: {
+          /* Gets the link to which you want to print */
           headTable = tableSearch(toks.at(i));
+          /* Sets the headpath to the new value */
           headTablePath = toks.at(i);
+          /* Checks to make sure it exists */
           if(headTable->table_name == "-1"){ toks.clear(); break; }
+          /* Sends in to the describeTable function in print.cc */
           describeTable(headTable);
+          /* Done! */
           break;
         }
 
@@ -159,17 +179,21 @@ int main(void){
          */
         case GET: {
           if(GET_DEBUG) printf("GET: %s\n", toks.at(i).c_str());
-
+          /* Tries to find the value you are searching for in the headTable */
           auto checksearch = headTable->data.find(toks.at(i));
 
           if(checksearch != headTable->data.end()){ /* found */
             if(GET_DEBUG) printf("Found: %s = %s\n", toks.at(i).c_str(), (checksearch->second.str_data).c_str());
+            /* Forms response if found */
             resp = ("Found '" + toks.at(i) + "' = '" + checksearch->second.str_data + "' in '" + headTablePath + "'");
           }else{ /* Did not find */
+            /* Forms response if not found */
             resp = "Did not find '" + toks.at(i) + "' in '" + headTablePath + "'";
             if(GET_DEBUG) printf("Did not find %s\n", toks.at(i).c_str());
           }
+          /* Sends response to formResponse in response.cc */
           formResponse(resp, stage);
+          /* Done! */
           break;
         }
 
@@ -184,15 +208,20 @@ int main(void){
          */
         case PUT: {
           if(PUT_DEBUG) printf("PUT: %s\n", toks.at(i).c_str());
-
+          /* Keys for the incoming Values */
           std::vector<std::string> keys;
+          /* values that map to those keys */
           std::vector<std::string> values;
-
+          /* used to know if i should be inserting into keys or values */
           bool isEqualsChar = true;
-
+          /* Starting a i because I dont want to read old tokens */
+          /* In general I wanted to keep away from reading past/future tokens
+             but I couldnt think of easier ways to get this done */
           for(int j = i; j < toks.size(); j++) {
+            /* Making sure the stage has not change, if it has we are in trouble */
             int temp = keyWordSearch(toks.at(j), stage);
             if(stage != temp){ stage = temp; break; }
+
             if(toks.at(i) == "="){
               isEqualsChar = false;
             }else if(isEqualsChar){
@@ -200,23 +229,31 @@ int main(void){
             }else{
               values.push_back(toks.at(i));
             }
+            /* Since i am reading future tokens I want to make sure I do
+               not read them later so i increment 'i' */
             i++;
           }
+          /* Start my response */
           resp = "Inserted";
           for(int j = 0; j < keys.size(); j++){
             resp += (" '" + keys.at(j) + "' = '" + values.at(j) + "'");
             auto search = headTable->data.find(keys.at(j));
             if(search != headTable->data.end()){ /* already created */
+              /* If the value you pair already exsists I just update it so we
+                 we dont experience any collisions of data in a link */
               search->second.str_data = values.at(j);
             }else{ /* not created */
               data_t newEntry;
               newEntry.str_data = values.at(j);
+              /* If the value pair has not been created already I need to create it
+                 and add it to the link */
               headTable->data.insert(std::make_pair(keys.at(j), newEntry));
             }
           }
           resp += " into '" + headTablePath + "'";
+          /* Sending my response to my reponse handler */
           formResponse(resp, stage);
-          // printf("%s", resp("Inserted\n").c_str());
+          /* Done! */
           break;
         }
 
@@ -228,20 +265,30 @@ int main(void){
          * This will go to Rhythm, into User, into Steven, and return his age if its there.
          */
         case TABLE_LOOKUP: {
+          /* Gets the link to which you want to print */
           headTable = tableSearch(toks.at(i));
+          /* Sets the headpath to the new value */
           headTablePath = toks.at(i);
+          /* Checks to make sure it exists */
           if(headTable->table_name == "-1"){ resp = "No such link as '" + toks.at(i) + "'";  formResponse(resp, stage); toks.clear(); }
+          /* Done! */
           break;
         }
 
         /* Create the new Database */
         case NEWDB: {
+          /* creats a new table_t */
           table_t bk;
+          /* Sets the name to the Database name */
           bk.table_name = toks.at(i);
           if(NEWDB_DEBUG) printf("NEW DB NAME:%s\n", toks.at(i).c_str());
+          /* insertings the name and the table as a pair into the database_table map */
           database_table.insert(std::make_pair(toks.at(i), bk));
+          /* Forms response */
           resp = "Database '" + toks.at(i) + "' was created!";
+          /* Sending my response to my reponse handler */
           formResponse(resp, stage);
+          /* Done! */
           break;
         }
       }
